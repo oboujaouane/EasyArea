02247610c62f44ee9a0f0f8a3b3028734b4d0a74
boujaouane <oboujaouane@gmail.com>
2021-07-11 17:55:50 +0400

    Initial Commit

---
+++ EasyArea/EasyAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyAreaApp.swift
+//  EasyArea
+//
+//  Created by ousama boujaouane on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
